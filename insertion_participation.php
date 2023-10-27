714b43113b16ad1987a762b5192abd29e4922353
Géraud Besson <triplegro.gb@gmail.com>
2023-10-27 14:56:04 +0200

    insertion

--- insertion_participation.php
+++ insertion_participation.php
@@ -15,33 +15,27 @@ if ($connexion->connect_error) {
 
 // Récupération des données du formulaire
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nomparticipant = $_POST["texte1"];
+    $nomparticipant = $_POST["nomparticipant"];
     $file = $_FILES["file"]["name"];
-    $exifs = $_POST["texte2"];
-    $funfact = $_POST["texte3"];
-
-
+    $exifs = $_POST["exifs"];
+    $funfact = $_POST["funfact"];
     
     echo "Nom participant: " . $nomparticipant . "<br>";
     echo "Nom du fichier: " . $file . "<br>";
     echo "Exifs: " . $exifs . "<br>";
     echo "Funfact: " . $funfact . "<br>";
+
+    // Requête SQL pour insérer les données dans la table "participants"
+    $requete = "INSERT INTO participants (nomparticipant, file, exifs, funfact) VALUES ('$nomparticipant', '$file', '$exifs', '$funfact')";
+
+    // Exécution de la requête
+    if ($connexion->query($requete) === TRUE) {
+        echo "Les données ont été insérées avec succès dans la table participants.";
+    } else {
+        echo "Erreur lors de l'insertion des données : " . $connexion->error;
+    }
+
+    // Fermer la connexion à la base de données
+    $connexion->close();
 }
-    // // Requête SQL pour insérer les données dans la table "participants"
-    // $requete = "INSERT INTO participants (nomparticipant, file, exifs, funfact) VALUES ('$nomparticipant', '$file', '$exifs', '$funfact')";}
-
-    // var_dump($nomparticipant, $file, $exifs, $funfact);
-    // echo $requete;
-
-    die();
-//     // Exécution de la requête
-//     if ($connexion->query($requete) === TRUE) {
-//         echo "Les données ont été insérées avec succès dans la table participants.";
-//     } else {
-//         echo "Erreur lors de l'insertion des données : " . $connexion->error;
-//     }
-
-//     // Fermer la connexion à la base de données
-//     $connexion->close();
-// }
 ?>
